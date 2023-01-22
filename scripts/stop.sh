fd4570d72177a4aece5a5d142d54cb58450957a8
DongchannN <ldc991104@gmail.com>
2023-01-22 20:11:08 +0900

    update start and stop scripts.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
+PROJECT_ROOT="/home/ec2-user/app/deploy"
+JAR_FILE="$PROJECT_ROOT/handsUp-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
