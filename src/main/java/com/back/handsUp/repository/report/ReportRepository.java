f7ea889102c735beed3e7ac9029f9519da9b257e
DongchannN <ldc991104@gmail.com>
2023-01-27 15:16:31 +0900

    유저 신고하기, 게시물 신고하기 분류

--- src/main/java/com/back/handsUp/repository/report/ReportRepository.java
+++ src/main/java/com/back/handsUp/repository/report/ReportRepository.java
@@ -1,9 +1,17 @@
 package com.back.handsUp.repository.report;
 
+import com.back.handsUp.domain.board.Board;
 import com.back.handsUp.domain.report.Report;
+import com.back.handsUp.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    boolean existsByUser(User user);
+
+    boolean existsByReportedUser(User reportedUser);
+
+    boolean existsByReportedBoard(Board reportedBoard);
 }
