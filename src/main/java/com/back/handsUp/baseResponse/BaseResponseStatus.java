fd0f40baf58479b51ee3f8d50122827249881d51
DongchannN <ldc991104@gmail.com>
2023-02-05 15:35:24 +0900

    fcmToken 관련 Response 추가

--- src/main/java/com/back/handsUp/baseResponse/BaseResponseStatus.java
+++ src/main/java/com/back/handsUp/baseResponse/BaseResponseStatus.java
@@ -18,7 +18,7 @@ public enum BaseResponseStatus {
     EXIST_USER(false, 4002, "이미 존재하는 유저입니다."),
     INVALID_REQUEST(false, 4003, "입력 양식이 잘못되었습니다."),
     SAME_PASSWORD(false, 4004, "현재 비밀번호와 변경할 비밀번호가 같습니다."),
-    LIMIT_NICKNAME_CHANGE(false,4005, "마지막 닉네임 변경일로부터 7일이 경과하지 않았습니다."),
+    LIMIT_NICKNAME_CHANGE(false, 4005, "마지막 닉네임 변경일로부터 7일이 경과하지 않았습니다."),
     EXIST_NICKNAME(false, 4006, "이미 존재하는 닉네임입니다."),
 
 
@@ -38,25 +38,25 @@ public enum BaseResponseStatus {
     LOCATION_ERROR(false, 4021, "위치정보에 오류가 발생했습니다."),
     MESSAGEDURATION_ERROR(false, 4022, "메세지 지속시간에 오류가 발생했습니다."),
 
-    NON_EXIST_CHARACTER_VALUE(false,4031,"캐릭터 생성에 필요한 값이 모두 입력되지 않았습니다."),
+    NON_EXIST_CHARACTER_VALUE(false, 4031, "캐릭터 생성에 필요한 값이 모두 입력되지 않았습니다."),
 
-    NON_EXIST_TAG_VALUE(false,4032,"유효하지 않은 테그값입니다."),
+    NON_EXIST_TAG_VALUE(false, 4032, "유효하지 않은 테그값입니다."),
 
-    REFRESH_TOKEN_ERROR(false,4040,"Refresh Token이 유효하지 않습니다."),
-    LOGOUT_USER(false,4041,"로그아웃된 사용자입니다."),
-    NOT_MATCH_TOKEN(false,4042,"토큰의 유저 정보가 일치하지 않습니다."),
-    MALFORMED_JWT(false,4043,"잘못된 JWT 서명입니다."),
-    EXPIRED_JWT(false,4044,"만료된 JWT 토큰입니다."),
-    UNSUPPORTED_JWT(false,4045,"지원되지 않는 JWT 토큰입니다."),
-    ILLEGAL_JWT(false,4046,"JWT 토큰이 잘못되었습니다."),
-    JWT_ERROR(false,4047,"JWT 토큰에 오류가 발생했습니다."),
-    ACCESS_DENIED_USER(false,4048,"접근 권한이 없는 유저입니다"),
+    REFRESH_TOKEN_ERROR(false, 4040, "Refresh Token이 유효하지 않습니다."),
+    LOGOUT_USER(false, 4041, "로그아웃된 사용자입니다."),
+    NOT_MATCH_TOKEN(false, 4042, "토큰의 유저 정보가 일치하지 않습니다."),
+    MALFORMED_JWT(false, 4043, "잘못된 JWT 서명입니다."),
+    EXPIRED_JWT(false, 4044, "만료된 JWT 토큰입니다."),
+    UNSUPPORTED_JWT(false, 4045, "지원되지 않는 JWT 토큰입니다."),
+    ILLEGAL_JWT(false, 4046, "JWT 토큰이 잘못되었습니다."),
+    JWT_ERROR(false, 4047, "JWT 토큰에 오류가 발생했습니다."),
+    ACCESS_DENIED_USER(false, 4048, "접근 권한이 없는 유저입니다"),
     SELF_REPORT_ERROR(false, 4049, "본인을 신고할 수 없습니다."),
     REPORTED_ERROR(false, 4050, "이미 신고가 접수되었습니다."),
     SELF_BLOCK_ERROR(false, 4051, "본인을 차단할 수 없습니다."),
     BLOCKED_USER_ERROR(false, 4052, "이미 차단된 유저입니다."),
     BLOCKED_BOARD_ERROR(false, 4053, "이미 차단된 게시물입니다."),
-
+    NON_EXIST_FCMTOKEN(false, 4054, "상대방의 FCM 토큰이 없습니다."),
 
 
     /**
@@ -64,11 +64,14 @@ public enum BaseResponseStatus {
      */
     DATABASE_INSERT_ERROR(false, 5000, "데이터베이스 저장 오류가 발생했습니다."),
     PASSWORD_ENCRYPTION_ERROR(false, 5001, "비밀번호 암호화 오류가 발생했습니다."),
-    EMAIL_SEND_ERROR(false, 5002, "이메일 인증번호 발송에 오류가 발생했습니다.");
+    EMAIL_SEND_ERROR(false, 5002, "이메일 인증번호 발송에 오류가 발생했습니다."),
+    PUSH_NOTIFICATION_SEND_ERROR(false, 5003, "알림을 보내는데 실패하였습니다."),
+    DATABASE_DELETE_ERROR(false, 5004, "데이터를 삭제하는데 오류가 발생하였습니다.");
 
     private final boolean isSuccess;
     private final int statusCode;
     private final String message;
+
     BaseResponseStatus(boolean isSuccess, int statusCode, String message) {
         this.isSuccess = isSuccess;
         this.statusCode = statusCode;
