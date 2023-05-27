1f90d13a9363758812027143c9495d3be57c04ac
slr-09 <ge12090@naver.com>
2023-05-27 18:45:52 +0900

    전체 게시물 조회 schoolName response 추

--- src/main/java/com/back/handsUp/dto/board/BoardDto.java
+++ src/main/java/com/back/handsUp/dto/board/BoardDto.java
@@ -52,8 +52,8 @@ public class BoardDto {
 
     @Builder
     public static class GetBoardList {
-//        @JsonProperty
-//        private String schoolName;
+        @JsonProperty
+        private String schoolName;
         @JsonProperty
         private List<BoardWithTag> getBoardList;
     }
